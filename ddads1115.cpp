8ba3bd64b39c60c8e02ec6697d947207306d8434
domoticsduino <info@domoticsduino.cloud>
2024-06-19 18:12:39 +0200

    Initial commit

---
+++ ddads1115.cpp
@@ -0,0 +1,41 @@
+/* 1.0.0 VERSION */
+
+#include "ddads1115.h"
+
+DDADS1115::DDADS1115(uint8_t gain = (uint8_t)0U){
+    this->_gain = gain;
+    Wire.begin();
+    if(!ADS.begin()){
+        if (SERIAL_ENABLED)
+            Serial.println("ADS not connected");
+    }
+    else{
+        if (SERIAL_ENABLED)
+            Serial.println("ADS connected");
+    }
+}
+
+DDADS1115Val DDADS1115::getValues(){
+    DDADS1115Val ret;
+    ADS.setGain(this->_gain);
+    float f = ADS.toVoltage(1); 
+    ret.anag1 = ADS.readADC(0);
+    ret.anag2 = ADS.readADC(1);
+    ret.anag3 = ADS.readADC(2);
+    ret.anag4 = ADS.readADC(3);
+    ret.volt1 = ret.anag1 * f;
+    ret.volt2 = ret.anag2 * f;
+    ret.volt3 = ret.anag3 * f;
+    ret.volt4 = ret.anag4 * f;
+    if (SERIAL_ENABLED){
+        Serial.print("A0: ");Serial.print(ret.anag1);
+        Serial.print(" A1: ");Serial.print(ret.anag2);
+        Serial.print(" A2: ");Serial.print(ret.anag3);
+        Serial.print(" A3: ");Serial.println(ret.anag4);
+        Serial.print("V0: ");Serial.print(ret.volt1);
+        Serial.print(" V1: ");Serial.print(ret.volt2);
+        Serial.print(" V2: ");Serial.print(ret.volt3);
+        Serial.println(" V3: ");Serial.println(ret.volt4);
+    }
+    return ret;
+}
---
+++ ddads1115.h
@@ -0,0 +1,34 @@
+/* 1.0.0 VERSION */
+
+#ifndef ads1115_h
+#define ads1115_h
+
+#include <SPI.h>
+#include "ADS1x15.h"
+
+#define SERIAL_ENABLED true
+
+struct DDADS1115Val{
+    int16_t anag1 = 0;
+    int16_t anag2 = 0;
+    int16_t anag3 = 0;
+    int16_t anag4 = 0;
+    float volt1 = 0.0;
+    float volt2 = 0.0;
+    float volt3 = 0.0;
+    float volt4 = 0.0;
+};
+
+class DDADS1115{
+
+    private:
+        ADS1115 ADS = ADS1115(0x48);
+        uint8_t _gain;
+
+    public:
+        DDADS1115(uint8_t);
+        DDADS1115Val getValues();
+
+};
+
+#endif
